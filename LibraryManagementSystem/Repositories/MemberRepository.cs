355bcbed40c9360d0caff7346381b8ca64884464
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-07-15 13:35:17 +0100

    chore: add method stubs for CRUD operations in MemberRepository

--- LibraryManagementSystem/Repositories/MemberRepository.cs
+++ LibraryManagementSystem/Repositories/MemberRepository.cs
@@ -7,6 +7,32 @@ namespace LibraryManagementSystem.Repositories
 	public class MemberRepository : BaseRepository
 	{
         public MemberRepository(IFileContext<Book> fileContext, string filePath = "members.json") : base(fileContext, filePath) { }
-	}
+
+        public List<Member> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetMemberById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member CreateMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member UpdateMember(Member member, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member DeleteMember(Guid id) {
+            throw new NotImplementedException();
+        }
+
+    }
+
 }
 
