2dfcdc8e3175091b88fc615739e39b9bcf5b7777
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-07-19 12:12:52 +0100

    chore: create method stubs with no implementation for MemberService before writing tests

---
+++ LibraryManagementSystem/Services/MemberService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem.Services
+{
+	public class MemberService
+	{
+		private MemberRepository _memberRepository;
+		
+		public MemberService(MemberRepository memberRepository)
+		{
+			_memberRepository = memberRepository;
+		}
+
+        public List<Member> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+		public Member RegisterMember(Member member)
+		{
+			throw new NotImplementedException();
+		}
+
+        public Member GetMember(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetMember(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member UpdateMember(Member member, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member DeleteMember(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
