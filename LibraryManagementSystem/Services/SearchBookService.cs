16a87288cb46ac545893a06ad79b91bac0786066
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-07-19 20:02:39 +0100

    chore: create method stub for searching books

---
+++ LibraryManagementSystem/Services/SearchBookService.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem;
+
+public class SearchBookService
+{
+    private BookRepository _bookRepository;
+    public SearchBookService(BookRepository bookRepository) {
+        _bookRepository = bookRepository;
+    }
+
+    public List<Book> SearchBooks(string searchTerm) {
+        throw new NotImplementedException();
+    }
+}
