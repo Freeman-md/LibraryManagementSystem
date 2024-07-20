c806bcf8f8473639908169d682581e5f0998bac0
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-07-20 12:53:09 +0100

    tests: setup test fixture and base class for Borrowing Transaction

---
+++ LibraryManagementSystem.Tests/TestHelpers/BorrowingTransactionRepositoryFixture.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementSystem.FileContexts;
+
+namespace LibraryManagementSystem.Tests;
+
+public class BorrowingTransactionRepositoryFixture : IDisposable
+{
+    public BorrowingTransactionRepository BorrowingTransactionRepository { get; set; }
+    private readonly string _testFilePath;
+
+    public BorrowingTransactionRepositoryFixture() {
+        _testFilePath = Path.Combine(Path.GetTempPath(), $"borrowing_transaction_repository_tests_{Guid.NewGuid()}.json");
+
+        JsonFileContext<BorrowingTransaction> fileContext = new JsonFileContext<BorrowingTransaction>();
+        BorrowingTransactionRepository = new BorrowingTransactionRepository(fileContext, _testFilePath);
+    }
+
+    public void Dispose() {
+        if (System.IO.File.Exists(_testFilePath)) 
+        {
+            System.IO.File.Delete(_testFilePath);
+        } 
+    }
+}
---
+++ LibraryManagementSystem.Tests/UnitTests/Repositories/BorrowingTransactionRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Tests;
+
+public class BorrowingTransactionRepositoryTests : IClassFixture<BorrowingTransactionRepositoryFixture>
+{
+    private readonly BorrowingTransactionRepository _borrowingTransactionRepository;
+    public BorrowingTransactionRepositoryTests(BorrowingTransactionRepositoryFixture fixture) {
+        _borrowingTransactionRepository = fixture.BorrowingTransactionRepository;
+    }   
+
+    private BorrowingTransaction CreateBorrowingTransaction(Book book, Member member, int borrowingDurationInDays = 7, double fine = 0.0, DateTime? returnDate = null) {
+        DateTime dueDate = DateTime.Now.AddDays(borrowingDurationInDays);
+
+        return new BorrowingTransaction(book, member, dueDate, fine, returnDate);
+    }
+}
