ccd4bfb9153fdae278a0cb50be55281ac1a763b7
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-07-21 15:53:36 +0100

    refactor: set constants for borrowing a book in borrowing transaction model

--- LibraryManagementSystem/Models/BorrowingTransaction.cs
+++ LibraryManagementSystem/Models/BorrowingTransaction.cs
@@ -5,9 +5,13 @@ namespace LibraryManagementSystem;
 
 public class BorrowingTransaction : Transaction
 {
+    
     public DateTime DueDate { get; private set; }
     public DateTime? ReturnDate { get; set; }
     public double Fine { get; set; }
+
+    public const int DEFAULT_BORROWING_DURATION_IN_DAYS = 7;
+    public const double FINE_RATE_PER_DAY = 10.0;
 
     [JsonConstructor]
     public BorrowingTransaction(Guid id, Book book, Member member, DateTime dueDate, double fine = 0.0, DateTime? returnDate = null)
